bcb993bb2dbeb6c0cc4d5f806d39bf7cbe204df6
duongghaa <duonggha13@gmail.com>
2020-03-30 22:31:22 +0700

    xử lý ấn phím chuyển câu

--- Tuan1_thudulieu/dataSpeech.py
+++ Tuan1_thudulieu/dataSpeech.py
@@ -1,62 +1,59 @@
 import os
 
 import nltk.data
-import getch
 import sounddevice as sd
 import soundfile as sf
-import time
-#import urllib.request
-
-'''urlpath = 'vnexpress.vn'
-urlopen = urllib.request.urlopen(urlpath)
-url = urlopen'''
-
+import queue
 
 topics = ['thoisu', 'gocnhin', 'thegioi', 'kinhdoanh', 'giaitri', 'thethao', 'phapluat', 'giaoduc', 'suckhoe', 'doisong', 'dulich', 'khoahoc', 'sohoa', 'xe', 'ykien', 'tamsu']
 
-def wait():
-    getch.getch()
+def readFile(filePathIn):
+	fin = open(filePathIn)
+	data = fin.read()
+	fin.close()
+	return data
+def separateSentences(data):
+	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+	sentences = tokenizer.tokenize(data)
+	return sentences
 
-def sync_record(filename, duration, fs, channels):
-    print('recording')
-    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
-    sd.wait()
-    sf.write(filename, myrecording, fs)
-    print('done recording')
+q = queue.Queue()
+def callback(indata, frames, time, status):
+	if status:
+		print(status)
+	q.put(indata.copy())
+
+def recording(filename):
+	try:
+		with sf.SoundFile(filename, mode='x', samplerate=22000, channels=1) as file:
+			with sd.InputStream(samplerate=22000, channels=1, callback=callback):
+				print('Start recording: ' + filename)
+				print('press Ctrl+C to next sentence')
+				while True:
+					file.write(q.get())
+	except KeyboardInterrupt:
+		print('Done recording!')
+		print('-' * 100)
+
 
 def main():
-	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
 	for topic in topics:
-		filePathIn = './in/' + topic +'.txt'
-		outPath = './out/' + topic
-		if not os.path.exists(outPath):
-			os.makedirs(outPath)
-		textPathOut = outPath + '/description.txt'
+		filePathIn = './in/' + topic + '.txt'
+		sentences = separateSentences(readFile(filePathIn))
 
-		fin = open(filePathIn)
-		data = fin.read()
-		fin.close()
+		textOutPath = './out/' + topic + '/index.txt'
 
-		sentences = tokenizer.tokenize(data)
-
-
-		fout = open(textPathOut, "w")
+		fout = open(textOutPath, "w")
 		fout.write(sentences[0] + '\n')
 
-		for sentence in sentences:
-			if(sentences.index(sentence)==0):
-				continue
+
+		for sentence in sentences[1:]:
 			print(sentence)
 			namerecord = topic + str(sentences.index(sentence)) + '.wav'
 			fout.write(namerecord + '\n')
 			fout.write(sentence + '\n')
-			sync_record(namerecord, 10, 16000, 1)
-
+			recording(namerecord)
 			wavOutPath = './out/' + topic + '/' + namerecord
 			os.rename(namerecord, wavOutPath)
-			#wait()
-		fout.close()
-
 if __name__ == '__main__':
 	main()
